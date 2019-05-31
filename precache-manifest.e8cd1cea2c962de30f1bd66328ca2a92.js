0236a6c8f60045cf0c40e1d2eb09e11dbb2b3b65
take8843 <1995.rohan@gmail.com>
2019-06-01 02:49:53 +0530

    Updates

---
+++ precache-manifest.e8cd1cea2c962de30f1bd66328ca2a92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "307d0d272a0881ee74e45b5e2e819f0f",
+    "url": "/take8843/bigapptasks.git/index.html"
+  },
+  {
+    "revision": "343caa0dd57d8d4a0556",
+    "url": "/take8843/bigapptasks.git/static/css/main.22f6edab.chunk.css"
+  },
+  {
+    "revision": "a7e9e8c6e6b2279cabca",
+    "url": "/take8843/bigapptasks.git/static/js/2.a005faa4.chunk.js"
+  },
+  {
+    "revision": "343caa0dd57d8d4a0556",
+    "url": "/take8843/bigapptasks.git/static/js/main.2f0a8171.chunk.js"
+  },
+  {
+    "revision": "2927d82e969b0176cc36",
+    "url": "/take8843/bigapptasks.git/static/js/runtime~main.e074d022.js"
+  }
+]);
